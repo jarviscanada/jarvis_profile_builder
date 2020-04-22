e36d614652a600c0c7cd8a5a56a482cd15c1d9e5
Edward Wang <edward@jrvs.ca>
2020-04-22 14:55:00 -0400

    update init

--- gen_resume.sh
+++ gen_resume.sh
@@ -2,22 +2,32 @@
 
 set -ex
 
+if [ "$1" = "init" ]; then
+  init
+fi
+
+init(){
+  chmod +x $0
+  wget https://raw.githubusercontent.com/jarviscanada/jarvis_resume_builder/master/resume_template.md -O ../README.md
+  bash gen_resume.sh
+  exit 1
+}
+
 if ! docker info &> /dev/null; then
   echo "Error: docker engine is not running"
   exit 1
 fi
 
-template_resume=resume_template.md
-output_resume=resume.md
+template_resume=../resume.md
+no_quote_resume=.resume.md
 output_resume_pdf=resume.pdf
-sed '/^>/d' $template_resume > $output_resume
+sed '/^>/d' $template_resume > $no_quote_resume
 
 top_bot_margin=1.75cm
 left_right_margin=1.5cm
 
 docker run --rm --volume "`pwd`:/data" --user `id -u`:`id -g` pandoc/latex:2.9.2.1 \
-    ${output_resume} -f markdown -t pdf -s \
+    ${no_quote_resume} -f markdown -t pdf -s \
     --pdf-engine=xelatex -V pagestyle=empty -V geometry:"top=${top_bot_margin}, bottom=${top_bot_margin}, left=${left_right_margin}, right=${left_right_margin}" -o ${output_resume_pdf}
 
-cp $output_resume ../README.md
 exit 0
