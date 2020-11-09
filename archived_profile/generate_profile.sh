51bede0a7f279ae71901e603c80265f857593de3
Edward Wang <edward@jrvs.ca>
2020-11-09 10:17:34 -0500

    update sid profile ;

---
+++ archived_profile/generate_profile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 /Users/ewang/dev/jrvs/jarvis_profile_builder/archived_profile/csv2profile.py /Users/ewang/dev/jrvs/jarvis_profile_builder/archived_profile/b2_profile.csv /Users/ewang/dev/jrvs/jarvis_profile_builder/archived_profile/profile_json_2
